210372d494e39310c855ec97e78ea2761e5d13fe
Ali Adel <aliadelali671@gmail.com>
2023-11-25 21:53:52 +0200

    init index router

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomePage from '../views/HomeView/Home.vue'
+import LoginPage from '../views/Login.vue'
+import HotelResultsPage from '../views/HotelsView/HotelResults.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: HomePage
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: LoginPage
+    },
+    {
+      path: '/hotelResults',
+      name: 'Hotel Results',
+      component: HotelResultsPage
+    }
+  ]
+})
+
+export default router
