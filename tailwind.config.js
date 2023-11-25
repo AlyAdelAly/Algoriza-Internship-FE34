9a2a9ae40605afeb42b05b61e4409962b57d25f0
Ali Adel <aliadelali671@gmail.com>
2023-11-25 21:45:58 +0200

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'light': '#F2F2F2',
+        'light-blue': '#2F80ED',
+        'orange-light': '#FCEFCA',
+        'light-gray': '#EBEBEB',
+        'gray': '#4F4F4F',
+        'light-black': '#181818'
+      },
+      fontSize: {
+        'sm': '13px',
+      },
+      borderRadius: {
+        'md': '4px',
+      }
+    },
+    container: {
+      padding: '100px',
+    },
+  },
+  plugins: [],
+}
